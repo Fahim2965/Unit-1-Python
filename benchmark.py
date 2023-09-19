4a3c5bf57c310b0419e6bd67a249bdc7fdf1b38b
Fahim Muntasir <famuntasir2@brooklynsteamcenter.org>
2023-09-19 09:50:36 -0400

    lets go

---
+++ benchmark.py
@@ -0,0 +1,49 @@
+# Exercise 1: 
+
+print("Fahim")
+
+
+# Exercise 2:
+
+favoriteShow = "Suits"
+
+
+# Exercise 3:
+
+height = 5.3
+age = 16
+print(height * age)
+
+# Exercise 4:
+
+print("im going to break")
+
+# Exercise 5:
+
+faveFoods = {"Chicken", "Pizza", "HALAL Food"}
+
+# Exercise 6:
+
+if age >= 21:
+    print("you can rent a car")
+else:
+    print("your beat")
+
+
+# Exercise 7:
+
+a = input("What is your name:")
+print("Hello " + a + "!")
+
+
+# Exercise 8:
+
+for foods in faveFoods:
+    print(foods)
+
+
+# Exercise 9:
+
+x = 4
+while x == 3:
+    print("Yes")
