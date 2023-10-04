f988a5e9d87731bceadec151a0b22ab2cfe8f848
Fahim Muntasir <famuntasir2@brooklynsteamcenter.org>
2023-10-04 10:33:46 -0400

    thgfder

--- todo-nottodo.py
+++ todo-nottodo.py
@@ -1,4 +1,15 @@
-todos = ["eat cake", "eaggs", "pan"]
-todos.append(input("Would you like to add more to your todo list: "))
-while 1 != 2:
+todos = []    # This empty list holds the users todos
+todos.append(input("What would you like to add to your todo list: ")) # This allows the user to add the tasks
+while True:  # The while loop is to continuously run the loop
     print("")
+    print(todos) # This is to print the todos
+    print("")
+    b = input("Do you want to add new tasks? Yes/No ") # This askes the user if they want add new todos
+    if b == "Yes" or b == "yes": 
+        todos.append(input("Add new task: ")) # This adds the new tods to the list
+    elif b == "No" or b == "no":
+        c = input("Do you want to remove a task? Yes/No ")
+        if c == "Yes" or c == "yes":
+            todos.remove(input("Which task would you like to remove: ")) # Thsi removes any tasks the user would like to remove
+        else:
+            continue # If theuseer does not chose anything the code will continue to run
