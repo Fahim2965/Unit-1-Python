6648e6e95d973d3533d6b64cf02afc65dca76fe9
Fahim <fahim.muntasir2965@gmail.com>
2023-10-16 21:42:58 -0400

    rtdf

--- todo-nottodo.py
+++ todo-nottodo.py
@@ -1,15 +1,39 @@
-todos = []    # This empty list holds the users todos
-todos.append(input("What would you like to add to your todo list: ")) # This allows the user to add the tasks
-while True:  # The while loop is to continuously run the loop
-    print("")
-    print(todos) # This is to print the todos
-    print("")
-    b = input("Do you want to add new tasks? Yes/No ") # This askes the user if they want add new todos
-    if b == "Yes" or b == "yes": 
-        todos.append(input("Add new task: ")) # This adds the new tods to the list
-    elif b == "No" or b == "no":
-        c = input("Do you want to remove a task? Yes/No ")
-        if c == "Yes" or c == "yes":
-            todos.remove(input("Which task would you like to remove: ")) # Thsi removes any tasks the user would like to remove
-        else:
-            continue # If theuseer does not chose anything the code will continue to run
+#Creats an empty variables for a list 
+Todo = []
+Choices = ''
+
+#Sets the continuation and stopping variables for the while loop
+while Choices != 'Stop':
+    print()
+    print('Your todos are:')
+    print(Todo)
+    print()
+#This gives the user choices to add, delete, or check the list.
+    Choices = input('Please select an option. "Add" : 1, "Delete" : 2 or "Stop" : 3 to stop:  ')
+    print()
+    if Choices == "Add" or Choices == "1" :
+#Asks user what they want to add to list
+        Addtodo = input('What would you like to add to your list: ')
+        Todo.append(Addtodo)
+        print()
+        print(Addtodo + ' was added!')
+        print()
+    
+#elif activates based on conditional (del varient)
+    elif Choices == "Delete" or Choices == "2" :
+        Deltodo = int(input("Please select the number for the item in the list you'd like to remove: "))
+#deletes specified item with -1 to make it user friendly on indexes
+        del Todo[Deltodo - 1]
+        print("Your selected statement has been deleted!")
+        print()
+
+#Stops loop if conditionals are met
+    elif Choices == "Stop" or Choices == "3" :
+        print('Statement is now stopping')
+        print()
+        break
+
+#Repeats the loop if no conditionals are met
+    else :
+        print('The answer chosen is invalid.')
+        print()
