e002bc1214349ead070421b81ddcf1624a339eb4
Fahim <109641484+Fahim2965@users.noreply.github.com>
2023-10-20 15:29:49 -0400

    pls dont fail me

--- classical_classes.py
+++ classical_classes.py
@@ -6,16 +6,23 @@ Then, write a method within the class to introduce the person with their name an
 Create a new object using this new class, and call the method you created.
 """
 
+# Created a class with the name Person
 class Person():
     
+    # init method for the name and age
     def __init__(self, name, age):
      self.name = name
      self.age = age
+
+     # Prints the name and age
     def hi(self):
        print(self.name)
        print(self.age)
 
+# Assigns class to a variable 
 Fahim = Person(str("Fahim"), 16)
+
+# Prints the hi method
 Fahim.hi()
     
 """
@@ -27,21 +34,28 @@ Then create two subclasses, Dog and Cat, each with their own speak method.
 Create objects using these subclasses and call the speak method.
 """
 
+# Initial class for the template
 class Animal():
+   # Method called speak to print what they say
    def speak(self):
     print()
 
+# Dog class using the animal template
 class Dog():
+  # Method called speak to print what they say
   def speak(self):
     print("Bark")
 
+# Calls the variables to print the speak method
 speak = Dog()
 speak.speak()
 
+# Cat class using the animal template
 class Cat():
   def speak(self):
     print("Meow")
 
+# Calls the variables to print the speak method
 speak = Cat()
 speak.speak()
 
@@ -55,4 +69,32 @@ Include methods for depositing and withdrawing money, which should modify the ba
 Test these methods with instances of the class.
 """
 
+# Creating class for bank account
+class BankAccount():
+    # innit method for all starter 
+    def __init__(self, balance, owner): 
+        self.balance = balance
+        self.owner = owner
+    
+    # Method for printing total balance in the bank
+    def total(self): 
+        print('Your balance is ' + str(self.balance))
 
+     # Method for adding deposit and printing total value
+    def deposit(self, depo):
+        self.balance = self.balance + depo
+        print('Your total balance is ' + str(self.balance))
+
+    # Method for substracting and withdraw then printing the total value
+    def withdraw(self, withd):
+        self.balance = self.balance - withd
+        print('Your total balance is ' + str(self.balance))
+
+# Assigning class to variable
+t3_bank = BankAccount(500, 'a')
+# Prints the total in balance
+t3_bank.total() 
+# Deposit method for adding and printing balance
+t3_bank.deposit(200)
+# Withdraw method for substracting and printing balance
+t3_bank.withdraw(100)
